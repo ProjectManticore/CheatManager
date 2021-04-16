b1b52972bd4ffc94091f2b60da46e9458727941c
evelyn <charlesdelorean1@icloud.com>
2021-04-16 08:35:30 -0400

    Initial Commit

---
+++ CheatManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CheatManager
+//
+//  Created by charles on 2021-04-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
